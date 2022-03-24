1ec4dd62e49e6dd53845b03491e7fff073eba84e
M1K3Hedges <73333304+M1K3Hedges@users.noreply.github.com>
2022-03-24 18:53:41 +0100

    Delete sirtah directory

--- sirtah/__init__.py
+++
@@ -1,7 +0,0 @@
-from .constants       import *
-from .pitchtrackers   import *
-from .audioprocessing import *
-from .yin             import *
-from .yin2            import *
-
-__all__ = [_ for _ in dir() if not _.startswith("_")]
